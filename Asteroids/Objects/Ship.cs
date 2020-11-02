e8fa4812f12c30bdcdffc390d725288f4a374747
Fenist <kostik06@gmail.com>
2020-11-02 12:21:17 +0300

    qwe

---
+++ Asteroids/Objects/Ship.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids.Objects
+{
+    class Ship : BaseObject
+    {
+        private int _energy = 100;
+        public int Energy => _energy;
+
+        public void EnergyLow(int n)
+        {
+            _energy -= n;
+        }
+        public Ship(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.FillEllipse(Brushes.Wheat, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+
+        }
+        public void UP()
+        {
+
+        }
+        public void Down()
+        {
+
+        }
+        public void Die()
+        {
+
+        }
+    }
+}
