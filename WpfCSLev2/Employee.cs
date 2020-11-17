7bae447c182c52d13117dd50d1f3e3f536f50051
Fenist <kostik06@gmail.com>
2020-11-17 03:34:53 +0300

    wpf

---
+++ WpfCSLev2/Employee.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfCSLev2
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _name;
+        private string _surname;
+        private string _patronymic;
+        private DateTime _birthday;
+        private byte _age;
+        private float _salary;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Surname
+        {
+            get => _surname;
+            set
+            {
+                _surname = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Patronymic
+        {
+            get => _patronymic;
+            set
+            {
+                _patronymic = value;
+                OnPropertyChanged();
+            }
+        }
+        public DateTime Birthday
+        {
+            get => _birthday;
+            set
+            {
+                _birthday = value;
+                OnPropertyChanged();
+            }
+        }
+        public byte Age
+        {
+            get => _age;
+            set
+            {
+                _age = value;
+                OnPropertyChanged();
+            }
+        }
+        public float Salary
+        {
+            get => _salary;
+            set
+            {
+                _salary = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ WpfCSLev2/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCSLev2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Employee> GetD { get; } = new ObservableCollection<Employee>();
+        public static ObservableCollection<Department> GetDepartment { get; } = new ObservableCollection<Department>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetD.Add(new Employee { Id = 1, Name = "Константин", Surname = "Носков", Patronymic = "Иванович", Birthday = new DateTime(1981, 12, 29), Age = 38, Salary = 120000 });
+            GetDepartment.Add(new Department { Name = "СКТ" });
+            GetDepartment.Add(new Department { Name = "бухгалтерия" });
+            GetDepartment.Add(new Department { Name = "водители" });
+        }
+
+        private void OnDragMoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else if (e.ClickCount == 2 && this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            this.DragMove();
+        }
+
+        private void CloseMenuBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenMenuBtn.Visibility = Visibility.Visible;
+            CloseMenuBtn.Visibility = Visibility.Collapsed;
+        }
+
+        private void OpenMenuBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenMenuBtn.Visibility = Visibility.Collapsed;
+            CloseMenuBtn.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = sender as ListViewItem;
+            //switch (item.Name)
+            //{
+            //    case "Administration":
+            //        DataContext = new MenuNavViewModel(1);
+            //        break;
+            //    case "Home":
+            //        DataContext = new MenuNavViewModel(0);
+            //        break;
+            //    case "Employees":
+            //        DataContext = new MenuNavViewModel(2);
+            //        break;
+            //    case "Scheduller":
+            //        DataContext = new MenuNavViewModel(3);
+            //        break;
+            //    case "Logout":
+            //        LoginWindow loginWindow = new LoginWindow();
+            //        loginWindow.Show();
+            //        this.Close();
+            //        break;
+            //}
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MainCC_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scrollViewer = (ScrollViewer)sender;
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+    }
+}
